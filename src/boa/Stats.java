b7a81264232a225306255ce7b2085b40b28a95e6
Zanaku <xzanaku@gmail.com>
2012-10-28 20:21:43 +0000

    modified comments to reflect source more accurately

--- src/boa/Stats.java
+++ src/boa/Stats.java
@@ -1,7 +1,7 @@
 package boa;
 
 public class Stats {
-	int health; //THIS IS UR HEALTH
+	int health; //THIS IS UR GOODS
 	int strength; //THIS IS UR STRONGS
 	int defence; //THIS IS UR BEEF
 	int speed; //THIS IS UR RUNS
